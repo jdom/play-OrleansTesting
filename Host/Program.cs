98c918bef6a624b2643df69b94d5b04ad3d513c6
Julian Dominguez <julido@microsoft.com>
2016-03-01 10:17:15 -0800

    Unit testing and host using run-time codegen

---
+++ Host/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using Orleans;
+using Orleans.Runtime.Configuration;
+using Orleans.Runtime.Host;
+
+namespace Host
+{
+    class Program
+    {
+        private static SiloHost siloHost;
+
+        static void Main(string[] args)
+        {
+            // Orleans should run in its own AppDomain, we set it up like this
+            AppDomain hostDomain = AppDomain.CreateDomain("OrleansHost", null,
+                new AppDomainSetup()
+                {
+                    AppDomainInitializer = InitSilo
+                });
+
+
+            DoSomeClientWork().Wait();
+
+            Console.WriteLine("Orleans Silo is running.\nPress Enter to terminate...");
+            Console.ReadLine();
+
+            // We do a clean shutdown in the other AppDomain
+            hostDomain.DoCallBack(ShutdownSilo);
+        }
+
+        static async Task DoSomeClientWork()
+        {
+            // Orleans comes with a rich XML configuration but we're just going to setup a basic config
+            var clientconfig = ClientConfiguration.LocalhostSilo();
+
+            GrainClient.Initialize(clientconfig);
+
+            var friend = GrainClient.GrainFactory.GetGrain<IHello>(0);
+            var result = await friend.SayHello("Goodbye");
+            Console.WriteLine(result);
+
+        }
+
+        static void InitSilo(string[] args)
+        {
+            var config = ClusterConfiguration.LocalhostPrimarySilo();
+            siloHost = new SiloHost(Dns.GetHostName(), config);
+
+            siloHost.InitializeOrleansSilo();
+            var startedok = siloHost.StartOrleansSilo();
+            if (!startedok)
+                throw new SystemException(String.Format("Failed to start Orleans silo '{0}' as a {1} node", siloHost.Name, siloHost.Type));
+
+        }
+
+        static void ShutdownSilo()
+        {
+            if (siloHost != null)
+            {
+                siloHost.Dispose();
+                GC.SuppressFinalize(siloHost);
+                siloHost = null;
+            }
+        }
+    }
+}
---
+++ Tests/HelloWorldSiloTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orleans.TestingHost;
+
+namespace Tests
+{
+    [TestClass]
+    public class HelloWorldSiloTests
+    {
+        private static TestingSiloHost host;
+
+        [ClassCleanup]
+        public static void StopOrleans()
+        {
+            // Optional. 
+            // By default, the next test class which uses TestignSiloHost will
+            // cause a fresh Orleans silo environment to be created.
+            host.StopAllSilos();
+        }
+
+        [TestInitialize]
+        public void InitializeOrleans()
+        {
+            if (host == null)
+            {
+                host = new TestingSiloHost(
+                    new TestingSiloOptions
+                    {
+                        StartSecondary = false,
+                    });
+            }
+        }
+
+        private static long GetRandomGrainId()
+        {
+            return new Random().Next();
+        }
+
+        [TestMethod]
+        public async Task SayHelloTest()
+        {
+            // The Orleans silo / client test environment is already set up at this point.
+
+            long id = GetRandomGrainId();
+            const string greeting = "Bonjour";
+
+            IHello grain = host.GrainFactory.GetGrain<IHello>(id);
+
+            // This will create and call a Hello grain with specified 'id' in one of the test silos.
+            string reply = await grain.SayHello(greeting);
+
+            Assert.IsNotNull(reply, "Grain replied with some message");
+            string expected = string.Format("You said: {0}, I say: Hello!", greeting);
+            Assert.AreEqual(expected, reply, "Grain replied with expected message");
+        }
+    }
+}
